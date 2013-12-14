177b7ea6a5eb63a25d5b3081b1f4444a9c312794
DSin52 <dsingh_52@hotmail.com>
2013-12-13 21:12:39 -0500

    Updated a couple of tests

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -10,7 +10,7 @@ public class Simulator {
 		sim.parseBusInfo("Stop_Times.csv");
 		
 		try {
-			sim.startSimulation(new String[] {"07:00:00 AM", "09:04:00 PM"}, new String[] {"07:01:23 AM", "11:15:00 AM"}, new String[] {"07:02:00 AM", "03:15:00 PM"});
+			sim.startSimulation(new String[] {"07:00:00 AM", "09:04:00 PM", "10:10:43 PM"}, new String[] {"07:01:23 AM", "11:15:00 AM", "08:30:13 AM"}, new String[] {"07:02:00 AM", "03:15:00 PM", "10:45:00 AM"});
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
